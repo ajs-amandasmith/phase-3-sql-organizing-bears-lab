3822b6bf3bbf856c0bac27a9e91355c58015ef91
Amanda Smith <ajs.amandasmith@gmail.com>
2022-08-04 20:07:32 -0500

    commit tree 3535139e999c077f81aba2c943b955c2547d047f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "feisty", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "F", "red", "mild", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "F", "brown", "mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Seargeant Brown", 6, "F", "black", "stoic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "M", "white", "peaceful", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "black", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "gray", "flighty", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "black", "passive", 1);
